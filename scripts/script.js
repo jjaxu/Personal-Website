2a212a4d2a7f4c86a0f0eff304c0fbb2857a0730
Jackie Xu <jackiexu2007@hotmail.com>
2017-04-30 23:02:04 -0400

    Reduced animation duration; added more jQuery effects; added pop-up message for other tabs.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,46 @@
 $(document).ready(function(){
+    $("#name").fadeIn(2000);
+    $("#title").fadeIn(2000);
+    $("#bar").fadeIn(2000);
+    $("#pics").fadeIn(2000);
+
+    var id;
+    var clr;
+
+    $('.about').on('mouseover', function() {
+        id = $(this).attr('id');
+
+        if (id == "fb") {
+            clr = 'blue';
+        } else if (id == 'gh') {
+            clr = 'purple';
+        } else if (id == 'li') {
+            clr = 'cyan';
+        } else {
+            clr = 'red';
+        }
+
+        $('#name').css(
+            {color: clr,
+            fontWeight:'bold'
+        });
+    });
+
+    $('.about').on('mouseleave', function() {
+        $('#name').css(
+            {color: 'black',
+            fontWeight:'normal'
+        });
+    });
+
+    $('.inactive').on('click',function() {
+        alert("Oops! This page is still under development");
+    });
+
+});
+
+/*
+$(document).ready(function(){
     $("#name").fadeIn(2000,function(){
         $("#title").fadeIn(2000);
         $("#bar").fadeIn(2000,function(){
@@ -6,3 +48,5 @@ $(document).ready(function(){
         });
     });
 });
+
+*/
