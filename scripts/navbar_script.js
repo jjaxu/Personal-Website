d515364e3733616a8266d3342e17fc6b45d2f2b2
Jackie Xu <jackiexu2007@hotmail.com>
2017-05-30 22:11:16 -0400

    Added module files

---
+++ scripts/navbar_script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var id;
+    var clr;
+    
+    $('.inactive').on('click', function() {
+        alert("Oops! This page is still under development");
+    });
+
+});
