ab281a6e76aeecbca7a04b46c6f83584ee441a39
Jackie Xu <jackiexu2007@hotmail.com>
2017-06-23 00:36:34 -0400

    Added modal popup for pages under development; this replaces the default not-so-good-looking alert

--- scripts/navbar_script.js
+++ scripts/navbar_script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('.inactive').on('click', function() {
-        alert("Oops! This page is still under development");
+        $('#myModal').modal('show');
     });
 });
