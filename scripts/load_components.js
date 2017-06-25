1adc7998a64c897bc99c9d451be8746f6e118d33
Jackie Xu <jackiexu2007@hotmail.com>
2017-06-24 23:13:39 -0400

    Added new pages (not yet active), removed some useless content, fixed navbar positioning

---
+++ scripts/load_components.js
@@ -0,0 +1,7 @@
+$(function(){
+  $("#navbar_template").load("/components/navbar.html");
+});
+
+$(function(){
+  $("#unfinished_modal").load("/components/unfinished_modal.html");
+});
--- scripts/navbar_script.js
+++ scripts/navbar_script.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
     $('.inactive').on('click', function() {
         $('#myModal').modal('show');
     });
+
+    /*
+    $('#about').on('click', function() {
+        $("#about").addClass("active");
+    });
+    */
+
 });
