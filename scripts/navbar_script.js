fa16f90c55bd68a135926f7907a47ee9cd74fdbf
Jackie Xu <jackiexu2007@hotmail.com>
2017-08-06 21:27:09 -0400

    Added javascript logic for the current page highlighting

--- scripts/navbar_script.js
+++ scripts/navbar_script.js
@@ -1,12 +1,25 @@
+var more_dropdown = ["gallery", "quotes", "time_capsule"];
+var page;
 $(document).ready(function(){
     $('.inactive').on('click', function() {
         $('#myModal').modal('show');
     });
 
+    if (window.location.pathname === "/") {
+      page = "home";
+    } else {
+      var path_arr = window.location.pathname.split("/");
+      page = path_arr[path_arr.length - 1].split(".html")[0];
+    }
+
+    $("#" + page).addClass("active");
+    if ($.inArray(page, more_dropdown) !== -1) {
+      $("#more").css("color", "yellow");
+    }
     /*
-    $('#about').on('click', function() {
-        $("#about").addClass("active");
+    $('#' + page).on('click', function() {
+        $("#" + page).addClass("active");
+        alert(page);
     });
     */
-
 });
